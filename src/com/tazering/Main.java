0269100d854f944ff844013af946f4f7a5e975ba
StormLok953 <tyler.ys.kim@gmail.com>
2021-06-08 18:44:59 -0400

    Generics

--- src/com/tazering/Main.java
+++ src/com/tazering/Main.java
@@ -6,19 +6,6 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<String> elements = new ArrayList<>();
-        elements.add("Botany");
-        elements.add("Aardvark");
-        elements.add("Anarchy");
-        elements.add("Retribution");
-        elements.add("Sovereignty");
-        elements.add("Hierarchy");
-        elements.add("Tempest");
-        elements.add("Tazering");
 
-        System.out.println((int)(Math.random() * 4));
-
-    }
 
 }
