5038f1dbdf6f461f23e72c08564533f351f1a544
StormLok953 <tyler.ys.kim@gmail.com>
2021-10-06 13:19:15 -0400

    commit

--- src/com/tazering/Main.java
+++ src/com/tazering/Main.java
@@ -4,8 +4,14 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Random;
 
 public class Main {
 
+    public static void main(String args[]) {
 
+        ArrayList<Double> observedValues = new ArrayList<>();
+
+
+    }
 }
---
+++ src/com/tazering/Microwave.java
@@ -0,0 +1,32 @@
+package com.tazering;
+
+public class Microwave {
+
+    private int timer;
+    private int temperature;
+
+    public void defrost() {
+
+        //defrost
+
+    }
+    
+    public int getTimer() {
+        return timer;
+    }
+
+    public void setTimer(int timer) {
+        this.timer = timer;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+
+
+}
