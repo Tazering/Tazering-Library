b10ba157371985cc8ae86ed2cc4888c097daf172
tkj9ep <tkj9ep@virginia.edu>
2022-08-25 13:02:00 -0400

    stuff

--- Data-Structures-and-Algorithms/Dynamic-Programming/String-Similarity.py
+++ Data-Structures-and-Algorithms/Dynamic-Programming/String-Similarity.py
@@ -1,8 +1,32 @@
+import numpy as np
+
 # Declare necessary variables
 firstWord = ""
 secondWord = ""
 
+# supplementary methods
+def calculateSimilarity(firstWord, secondWord):
 
+    dynamicProgrammingGrid = np.zeros((len(firstWord), len(secondWord)))
+
+    for i in range(dynamicProgrammingGrid.shape[0]):
+        if(firstWord[i] == secondWord[i]):
+            if(i == 0):
+                dynamicProgrammingGrid[i][i] = 1
+            else:
+                dynamicProgrammingGrid[i][i] = dynamicProgrammingGrid[i-1][i-1] + 1
+    
+    return dynamicProgrammingGrid
+
+def print2dList(arr):
+    for i in range(arr.shape[0]):
+        row = ""
+        for j in range(arr.shape[1]):
+            row = row + " " + str(arr[i][j])
+        print(row)
+
+
+# main method
 def main():
     print("This computes how similar the strings are to each other.")
     print("Input two words to compare:")
@@ -10,9 +34,11 @@ def main():
     firstWord = input()
     print("Type the second word")
     secondWord = input()
-
     print("The two words you typed were " + firstWord + " and " + secondWord)
 
+    arr = calculateSimilarity(firstWord, secondWord)
+    #print2dList(arr)
+    print(np.amax(arr))
 
 
 
---
+++ Documentation/Matlab.md
@@ -0,0 +1,12 @@
+# ***Matlab Documentation***
+
+## **Overview**
+Matlab is a high-level programming language and interactive environment for numerical computation, visualization, and programming. The term *matlab* means *Matrix Laboratory*. Matlab has a variety application ranging from image processing, biomedicine, machine learning and many others.
+
+## **Installations**
+
+## **Syntax**
+
+## **Resources**
+
+[Matlab Documentation](https://www.mathworks.com/help/matlab/)
